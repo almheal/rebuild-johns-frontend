e425305c88083fb6e6dab4e41b19b60c4f28b7bc
Dmitry <yellowblackyellow123@gmail.com>
2021-11-10 10:23:08 +0300

    finish constructor.spec

---
+++ tests/unit/__mocks__/ingredients.mock.js
@@ -0,0 +1,18 @@
+export const ingredientsMock = [
+  {
+    _id: '61544c84b45ffc71bcf64a9e',
+    category: 'app.ingredients.categories.vegetables',
+    price: '89',
+    icon: 'http://res.cloudinary.com/dimdjrct2/image/upload/v1632914562/xibbdwjo2kehwin8x0uh.svg',
+    title: 'app.ingredients.items.spinach',
+    __v: 0,
+  },
+  {
+    _id: '61544ca7b45ffc71bc',
+    category: 'app.ingredients.categories.vegetables',
+    price: '89',
+    icon: 'http://res.cloudinary.com/dimdjrct2/image/upload/v1632914597/vgoctajtkzovkrezyy76.svg',
+    title: 'app.ingredients.items.champignon',
+    __v: 0,
+  },
+]
