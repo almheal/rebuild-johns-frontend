d47cf4e36956156bccea366ce3ab207ce62c61d7
Dmitry <yellowblackyellow123@gmail.com>
2021-11-11 16:27:15 +0300

    product-card.spec

--- tests/unit/__mocks__/ingredients.mock.js
+++ tests/unit/__mocks__/ingredients.mock.js
@@ -16,3 +16,12 @@ export const ingredientsMock = [
     __v: 0,
   },
 ]
+
+export const ingredientMock = {
+  _id: '61544c84b45ffc71bcf64a9e',
+  category: 'app.ingredients.categories.vegetables',
+  price: '89',
+  icon: 'http://res.cloudinary.com/dimdjrct2/image/upload/v1632914562/xibbdwjo2kehwin8x0uh.svg',
+  title: 'app.ingredients.items.spinach',
+  __v: 0,
+}
--- tests/unit/jest.init.js
+++ tests/unit/jest.init.js
@@ -1,3 +1,4 @@
 import { config } from '@vue/test-utils'
 
 config.global.mocks.$t = (msg) => msg
+config.global.directives.lazy = () => {}
